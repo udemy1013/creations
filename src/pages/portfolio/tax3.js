37745e52e6865cd0797d52f04a3538d00dcb3ce0
RyoYoshikawa <udemy1013@gmail.com>
2023-05-31 20:53:06 +0900

    fixed overflow problem

--- src/pages/portfolio/tax3.js
+++ src/pages/portfolio/tax3.js
@@ -13,43 +13,6 @@ import useLocationChange from '../../components/useLocationChange.ts'
 
 const Tax3 = () => {
 
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-    const bodyElement = document.body;
-    const rootElement = document.getElementById("root");
-
-    const prevHtmlOverflow = htmlElement.style.overflow;
-    const prevBodyOverflow = bodyElement.style.overflow;
-    const prevRootOverflow = rootElement ? rootElement.style.overflow : null;
-
-    htmlElement.style.overflow = "visible";
-    bodyElement.style.overflow = "visible";
-    if (rootElement) {
-      rootElement.style.overflow = "visible";
-    }
-
-    // ComponentWillUnmountの代わりとして
-    return () => {
-      htmlElement.style.overflow = prevHtmlOverflow;
-      bodyElement.style.overflow = prevBodyOverflow;
-      if (rootElement) {
-        rootElement.style.overflow = prevRootOverflow;
-      }
-    };
-  }, []); // 空配列を依存性リストとして渡すことで一度だけ実行する
-
-    useLocationChange((location) => {
-        const removeOverflowHidden = (className) => {
-            const elements = document.getElementsByClassName(className);
-      
-            for (let i = 0; i < elements.length; i++) {
-              elements[i].style.overflow = 'visible';
-            }
-          };
-      
-          removeOverflowHidden('sc-beqWaB hPGFqH');
-          removeOverflowHidden('sc-gueYoa xAJSw');
-      })
   return (
     <Container 
     sx={{ maxWidth: "820px !important", margin: "auto", alignItems: "flex-end", letterSpacing: "0.18em", mt: 5, p:"24px" , pr:"0 !important", pl:"0 !important" }}>
